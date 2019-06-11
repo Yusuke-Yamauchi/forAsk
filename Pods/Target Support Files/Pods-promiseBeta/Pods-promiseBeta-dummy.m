dba32cd394d95dd1f69fa4480352c8b2e7d10562
junteak <edamame.zombie@gmail.com>
2019-06-11 15:33:01 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-promiseBeta/Pods-promiseBeta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_promiseBeta : NSObject
+@end
+@implementation PodsDummy_Pods_promiseBeta
+@end
---
+++ promiseBeta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  promiseBeta
+//
+//  Created by 潤田中 on 2019/06/11.
+//  Copyright © 2019 BCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
