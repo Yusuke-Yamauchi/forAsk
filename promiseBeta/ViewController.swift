3f40eb3e0dec242fb28ce9bc0b86282b3249014a
junteak <edamame.zombie@gmail.com>
2019-06-12 14:30:53 +0900

    2nd commit

--- promiseBeta/ViewController.swift
+++ promiseBeta/ViewController.swift
@@ -1,20 +1,26 @@
-//
-//  ViewController.swift
-//  promiseBeta
-//
-//  Created by 潤田中 on 2019/06/11.
-//  Copyright © 2019 BCC. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        
+    }
+    
+    @IBOutlet weak var textPromiseName: UITextField!
+    
+
+ 
+    
+    
+
+    @IBOutlet weak var textYourName: UITextField!
+    
+    
+    
+
+    
+    
 
 }
-
