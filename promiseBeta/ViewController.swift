f5f95668eaa68cf8d92d602dfe5b125ce6853fd1
junteak <edamame.zombie@gmail.com>
2019-06-13 00:42:24 +0900

    透明作る前

--- promiseBeta/ViewController.swift
+++ promiseBeta/ViewController.swift
@@ -8,48 +8,45 @@ class ViewController: UIViewController {
         
     }
     
+    //詳細を保存する配列を代入する変数
     
-    //Promise Name の詳細を保存する配列を代入する変数
     var data: [[String:Any]] = [[:]]
 
-    //Detailを入力する時にあらわれるOverView
-    @IBOutlet weak var overViewAdd: UIView!
-  
     
     
+    //promiseNameのText
     
-    //約束の名前
-    @IBOutlet weak var textPromiseName: UITextField!
-  
-    //詳細の入力画面を呼び出す
-    @IBAction func openDetailText(_ sender: Any) {
-    }
-    
-    //詳細入力の半透明のまView
-    @IBOutlet weak var overViewDetail: UIView!
-    
-    //Detailを入力したText
+    @IBOutlet weak var promiseNameText: UITextField!
     @IBOutlet weak var detailText: UITextField!
-    
-    
-    @IBAction func detail(_ sender: Any) {
-        
-//        detail.text! = "詳細に書かれた頭何文字か保存して半透明overViewDetailを閉じる"
-        
-    }
-    
-    
+    @IBOutlet weak var promiseDateText: UITextField!
+    @IBOutlet weak var dueDateText: UITextField!
+    @IBOutlet weak var yourNameText: UITextField!
+    @IBOutlet weak var partnerNameText: UITextField!
 
-   //自分の名前、自動入力？
-    @IBOutlet weak var textYourName: UITextField!
+
     
-    //サイン入力の画面へ遷移
-    @IBAction func next(_ sender: Any) {
-        
-        
-        
-        
+    
+    
+    
+    
+    
+    
+    @IBAction func detailButton(_ sender: Any) {
     }
+    
+    
+    @IBAction func partnerNameButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
